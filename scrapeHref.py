55418c8ab66bb2bbfbb136c9010123fc5509440d
AtomicCons <tm4113@gmail.com>
2019-01-07 23:56:41 -0700

    Scraping works
    tested sites with links utilizing various different file locations.

--- scrapeHref.py
+++ scrapeHref.py
@@ -12,22 +12,22 @@ urllib3.disable_warnings()
 
 #change directory
 home = os.environ['HOME']
-#folder = input('Name a folder: ')
+folder = input('Name a folder: ')
 
 #check if direcotry exists
-# if os.path.exists(home+'/'+folder) == False:
-#     os.mkdir(home+'/'+folder)
-# os.chdir(home+'/'+folder)
-
+if os.path.exists(home+'/'+folder) == False:
+    os.mkdir(home+'/'+folder)
+os.chdir(home+'/'+folder)
+print('current dir: '+os.getcwd())
 #sets array variables
-fileArr = []
 linkArr = []
 urlArr = []
 
 #sets url to get data
 url = input('URL containing PDF links: ')
-if url.startswith('http') == False:
-    url = input('Please put the URL with the protocol.. hint. https:// or http://')
+
+while url.startswith('http') == False:
+    url = input('Please put the URL with the protocol.. hint. https:// or http:// : ')
 
 urlArr = url.split('/')
 
@@ -35,22 +35,18 @@ urlArr = url.split('/')
 protocol = urlArr[0]
 site = urlArr[2]
 
-
 #get data
 response = requests.get(url,verify=False)
-#print(response.text)
+
 #sets htmlParser Params
 from html.parser import HTMLParser
 class dataParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
-            if attrs[0][1].endswith('pdf'):
-                linkArr.append(attrs[0][1])
-    #def handle_data(self, data):
-    #    pdf = data.endswith('pdf')
-    #    if pdf == True:
-    #        print('File Found: '+data + '. Adding file to list.')
-    #        fileArr.append(data)
+            for name, value in attrs:
+                if name == "href" and value.endswith('pdf'):
+                    print('Found '+value+'. Adding to que')
+                    linkArr.append(value)
 
 #parses data
 parser = dataParser()
@@ -58,31 +54,28 @@ parser.feed(response.text)
 for item in linkArr:
     iArr = item.split('/')
     filename = iArr[-1]
-    if item.startswith('htt'):
+    if item.startswith('http'):
         link = item
-        print('filename: ' + filename + '   whole link  Link: '+ link)
     elif item.startswith('.'):
         link = url+item
-        print('filename: ' + filename + ' starts with .  Link: '+ link)
     elif item.startswith('/'):
-        link = protocol+site+item
-        #link = link.replace('//','/')
-        print('filename: '+filename + 'starts with /  Link: '+link)
+        path = site+'//'+item
+        path = path.replace('//','/')
+        link = protocol+'//'+path
     else:
-        print(url+'/'+filename + ' not sure')
+        link = protocol+'//'+site+'/'+item
+        print('no prefix logic. Trying: '+link)
 
+    if os.path.exists(item):
+        print(item + 'exsits; skipping.')
+    else:
+        print('starting download of '+ item)
+        itemURL = link
 
-#checks if file exists.Downloads file if it doesn't
-# for item in fileArr:
-#     if os.path.exists(item):
-#         print(item+' exists. Skipping file.')
-#     else:
-#         print('starting download of '+ item)
-#         itemURL = site+'/'+item
-#         def download_file(itemURL):
-#             filename = item
-#             r = requests.get(itemURL, stream=True)
-#             with open(filename, 'wb') as f:
-#                 shutil.copyfileobj(r.raw, f)
-#             return filename
-#         download_file(itemURL)
+        def download_file(itemURL):
+            r = requests.get(itemURL, stream=True)
+            with open(filename, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+            return filename
+        download_file(itemURL)
+        print(filename+' saved')
